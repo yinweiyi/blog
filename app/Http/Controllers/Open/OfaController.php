f1419294e6c0e6f1e4dbb76c94d505bd6e9f4a32
wayee <378772944@qq.com>
2023-05-10 18:03:00 +0800

    pref: test receipt

--- app/Http/Controllers/Open/OfaController.php
+++ app/Http/Controllers/Open/OfaController.php
@@ -3,17 +3,45 @@
 namespace App\Http\Controllers\Open;
 
 use App\Http\Controllers\Controller;
+use EasyWeChat\Kernel\Exceptions\BadRequestException;
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\Kernel\Exceptions\RuntimeException;
+use EasyWeChat\OfficialAccount\Application;
+use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
 
 class OfaController extends Controller
 {
+    private Application $ofaApp;
 
-    public function auth()
+    public function __construct()
     {
-        return app('wechat.ofa')->getServer()->serve();
+        $this->ofaApp = app('wechat.ofa');
     }
 
+    /**
+     * @throws InvalidArgumentException
+     * @throws \Throwable
+     * @throws \ReflectionException
+     * @throws BadRequestException
+     * @throws RuntimeException
+     */
+    public function auth(): ResponseInterface
+    {
+        return $this->ofaApp->getServer()->serve();
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws \Throwable
+     * @throws \ReflectionException
+     * @throws BadRequestException
+     */
     public function receive()
     {
-        return 'ok';
+        $decryptedMessage = $this->ofaApp->getServer()->getDecryptedMessage();
+        $requestMessage = $this->ofaApp->getServer()->getRequestMessage();
+
+        Log::info('decryptedMessage:, ' . $decryptedMessage->toJson() . "\nrequestMessage:" . $requestMessage->toJson());
     }
 }
