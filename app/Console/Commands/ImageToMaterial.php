903d57ae9723ff53b3e713e83f0d7759a2ea243e
wayee <378772944@qq.com>
2023-05-12 18:59:52 +0800

    pref: image to material script

---
+++ app/Console/Commands/ImageToMaterial.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Image;
+use App\Vendors\File\OSS;
+use EasyWeChat\Kernel\Form\File;
+use EasyWeChat\Kernel\Form\Form;
+use EasyWeChat\OfficialAccount\Application;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ImageToMaterial extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'image:to_material';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Image table image_url to official account material';
+
+    /**
+     * @var Application|\Illuminate\Contracts\Foundation\Application|mixed
+     */
+    private Application $ofaApp;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ofaApp = app('wechat.ofa');
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $images = Image::query()->where('material_id', '')->orderByDesc('id')->select('id', 'image_url')->get();
+
+        $ossConfig = Config::get('oss');
+
+        foreach ($images as $image) {
+            if (!Str::startsWith($image->image_url, $ossConfig['domain'])) {
+                continue;
+            }
+            $this->info('Transform ：' . $image->image_url);
+
+            $path = Str::replace($ossConfig['domain'] . '/storage/', '', $image->image_url);
+            $storagePath = Storage::path($path);
+            if (!is_file($storagePath)) {
+                continue;
+            }
+            $this->info('正在处理：' . $image->image_url);
+
+            $options = Form::create(
+                [
+                    'media' => File::fromPath($storagePath)
+                ]
+            )->toArray();
+
+            $options['query'] = ['type' => 'image'];
+
+            $response = $this->ofaApp->getClient()->post('/cgi-bin/material/add_material', $options);
+
+            $content = \json_decode($response->getContent(), true);
+
+            if ($mediaId = Arr::get($content, 'media_id')) {
+                $image->fill(['media_id' => $mediaId])->save();
+                $this->info('处理成功。。。');
+            } else {
+                $this->error('处理失败。。。');
+            }
+        }
+    }
+}
---
+++ database/migrations/2023_05_12_184527_update_images_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->string('material_id', 100)->after('negative_prompt')->default('')->comment('素材id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('images', ['material_id']);
+    }
+};
